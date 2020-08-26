a53cad47d07ca48a0a518e41bfd2db4726bbcc01
Jae <ghostsintowers@gmail.com>
2020-08-26 09:49:03 -0500

    Making responsive - single palette screen

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,10 +15,11 @@ export default {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: (props) => (props.showingFullPalette ? '20%' : '50%')
+      height: (props) => (props.showingFullPalette ? '20%' : '33.33333%')
     },
     [sizes.down('md')]: {
-      width: '50%'
+      width: '50%',
+      height: (props) => (props.showingFullPalette ? '10%' : '20%')
     },
     [sizes.down('xs')]: {
       width: '100%',
@@ -116,7 +117,10 @@ export default {
       textAlign: 'center',
       marginBottom: '0',
       padding: '1rem',
-      textTransform: 'uppercase'
+      textTransform: 'uppercase',
+      [sizes.down('xs')]: {
+        fontSize: '5rem'
+      }
     },
     '& p': {
       fontSize: '2rem',
